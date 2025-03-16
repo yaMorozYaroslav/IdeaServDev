204db909149b27ad502f6ee93af62ec48a0278c4
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2025-03-16 00:42:43 -0300

    07

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(cors({
   //~ origin: 'http://localhost:3000',
   origin: ["https://idea-sphere-50bb3c5bc07b.herokuapp.com",
            "http://localhost:3000"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
 
