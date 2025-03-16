d8f8e55a0cdfdefc12a40743e48525f5cad14b5b
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2025-03-16 00:11:43 -0300

    06

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(cookieParser());
 app.use(cors({
   //~ origin: 'http://localhost:3000',
-  origin: "*",
+  origin: ["https://idea-sphere-50bb3c5bc07b.herokuapp.com",
+           "http://localhost:3000"],
   credentials: true
 }));
 
