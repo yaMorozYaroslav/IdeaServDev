77adee3c835b37b7180e27deb118b851d93fd664
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2025-03-17 12:02:12 -0300

    12

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const app = express();
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(cookieParser());
 app.use(cors({
-  origin: '*',
-  //~ origin: ["https://idea-sphere-50bb3c5bc07b.herokuapp.com",
-           //~ "http://localhost:3000"],
+  //~ origin: '*',
+  origin: ["https://idea-sphere-50bb3c5bc07b.herokuapp.com",
+           "http://localhost:3000"],
   
   credentials: true
 }));
